037e8eee7af11d13234cda7da43d4a31f3f25087
Cam Moore <cammymoore07@gmail.com>
2024-07-16 15:34:06 -1000

    Update signin-signout.spec.ts

--- tests/signin-signout.spec.ts
+++ tests/signin-signout.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('Signin and Signout', async ({ page }) => {
+  test.slow();
   await page.goto('http://localhost:3000/');
   await page.getByRole('button', { name: 'Login' }).click();
   await page.getByRole('link', { name: 'Sign in' }).click();
