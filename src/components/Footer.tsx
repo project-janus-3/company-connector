8a721ee7a7d105c697d68a6c279ffdb9124240f6
rycejrvy <goat.rj23@gmail.com>
2025-04-10 09:14:51 -1000

    Edited Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Col, Container, Row } from 'react-bootstrap';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
-  <footer className="mt-auto py-3 bg-light">
+  <footer className="mt-auto py-3 bg-light footer-text footer-style">
     <Container>
       <Row>
         <Col xs={2} className="text-center">
@@ -10,6 +10,11 @@ const Footer = () => (
         </Col>
         <Col xs={8} className="text-center">
           <a href="https://github.com/project-janus-3">GitHub Page</a>
+        </Col>
+        <Col>
+          &copy;
+          {new Date().getFullYear()}
+          &nbsp;Project Janus
         </Col>
       </Row>
     </Container>
