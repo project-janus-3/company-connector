84339d7be060899fa2ad3e9d31440c95ab2780c2
Danny Ngo <166663970+danny-ngo-2005@users.noreply.github.com>
2025-04-15 09:50:41 -1000

    Update page.tsx

--- src/app/company/page.tsx
+++ src/app/company/page.tsx
@@ -1,30 +1,41 @@
-import { getServerSession } from 'next-auth';
-import { Col, Container, Row, Table } from 'react-bootstrap';
-import { prisma } from '@/lib/prisma';
-import { adminProtectedPage } from '@/lib/page-protection';
-import authOptions from '@/lib/authOptions';
+import Image from 'next/image';
 
-const CompanyProfile = async () => {
+const CompanyProfile = async () => (
   <main className="container my-5">
     <div className="row mb-5">
-        <div className="card p-3 shadow-sm profile-sidebar-content">
-            <div className="d-flex align-items-center mb-4">
-                <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/free-company-logo-design-template-f02206903dcc70505736cca7fd9c5104_screen.jpg?ts=1638867492"
-                      alt="Profile Picture" 
-                      className="img-fluid rounded-circle me-3" 
-                      style= {{ width: 150, height: 150 }} />
-                <div>
-                    <p className="mb-1 fw-bold">COMPANY NAME</p>
-                    <p className="mb-0">Location: Foo</p>
-                    <div className="card p-3 shadow-sm">
-                        <h5>OVERVIEW</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
-                </div>
+      <div className="card p-3 shadow-sm profile-sidebar-content">
+        <div className="d-flex align-items-center mb-4">
+          <Image
+            src={
+              'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/'
+              + 'free-company-logo-design-template-f02206903dcc70505736cca7fd9c5104_screen.jpg?ts=1638867492'
+            }
+            alt="Company logo"
+            className="img-fluid rounded-circle me-3"
+            width={150}
+            height={150}
+          />
+          <div>
+            <p className="mb-1 fw-bold">COMPANY NAME</p>
+            <p className="mb-0">Location: Foo</p>
+            <div className="card p-3 shadow-sm">
+              <h5>OVERVIEW</h5>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit
+                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+              </p>
             </div>
+          </div>
         </div>
       </div>
-    </main>
-};
+    </div>
+  </main>
+);
 
 export default CompanyProfile;
