2c7c4a6e98f5d9d493dc8543520eff633892058d
Danny Ngo <166663970+danny-ngo-2005@users.noreply.github.com>
2025-04-14 18:30:20 -1000

    company-account mockup base

--- src/app/company/page.tsx
+++ src/app/company/page.tsx
@@ -1,68 +1,44 @@
 import { getServerSession } from 'next-auth';
 import { Col, Container, Row, Table } from 'react-bootstrap';
-import StuffItemCompany from '@/components/StuffItemCompany';
 import { prisma } from '@/lib/prisma';
 import { adminProtectedPage } from '@/lib/page-protection';
 import authOptions from '@/lib/authOptions';
 
-const CompanyPage = async () => {
-  const session = await getServerSession(authOptions);
-  adminProtectedPage(
-    session as {
-      user: { email: string; id: string; randomKey: string };
-    } | null,
-  );
-  const stuff = await prisma.stuff.findMany({});
-  const users = await prisma.user.findMany({});
+const CompanyProfile = async () => {
+  <main className="container my-5">
+    <div className="row mb-5">
+      <div className="col-md-4 profile-sidebar">
+        <div className="card p-3 shadow-sm profile-sidebar-content">
+          <img src="https://static-00.iconduck.com/assets.00/profile-default-icon-2048x2045-u3j7s5nj.png" alt="Profile Picture" className="img-fluid rounded-circle mb-3" style={{ width: '150px', height: '150px' }} />
+          <p>Company Name</p>
+          <p>Location: Foo</p>
+          <div className="card p-3 shadow-sm">
+          <h5>OVERVIEW</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+          <a href="#" style={{ width: '5%' }}>Edit Profile</a>
+        </div>
+      </div>
+      <div className="col-md-8 profile-contents">
+        <div className="row">
+          <div className="col-7">
+            <div className="card p-3 mt-4 shadow-sm">
+              <h5>INTERESTS-SKILLS</h5>
+              <p>Lorem ipsum</p>
+            </div>
+          </div>
 
-  return (
-    <main>
-      <Container id="list" fluid className="py-3">
-        <Row>
-          <Col>
-            <h1>List Stuff Company</h1>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Quantity</th>
-                  <th>Condition</th>
-                  <th>Owner</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {stuff.map((item) => (
-                  <StuffItemCompany key={item.id} {...item} />
-                ))}
-              </tbody>
-            </Table>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h1>List Company</h1>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Email</th>
-                  <th>Role</th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.map((user) => (
-                  <tr key={user.id}>
-                    <td>{user.email}</td>
-                    <td>{user.role}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          </Col>
-        </Row>
-      </Container>
-    </main>
-  );
+          <div className="col-5">
+            <div className="card p-3 mt-4 shadow-sm">
+              <h5>PROFESSIONAL PORTFOLIO</h5>
+              <a href="#">Lorem ipsum</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </main>;
 };
 
-export default CompanyPage;
+export default CompanyProfile;
