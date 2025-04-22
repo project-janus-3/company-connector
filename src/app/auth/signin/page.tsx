9676a44180ae1c8e872b360695092cc145f004b7
rycejrvy <goat.rj23@gmail.com>
2025-04-22 10:12:15 -1000

    Updated the sign-in page

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { signIn } from 'next-auth/react';
-import { Button, Card, Col, Container, Form, Row } from 'react-bootstrap';
+import { Button, Card, Col, Container, Form, Row, CardGroup } from 'react-bootstrap';
 
 /** The sign in page. */
 const SignIn = () => {
@@ -29,28 +29,41 @@ const SignIn = () => {
       <Container>
         <Row className="justify-content-center">
           <Col xs={5}>
-            <h1 className="text-center">Sign In</h1>
-            <Card>
-              <Card.Body>
-                <Form method="post" onSubmit={handleSubmit}>
-                  <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email</Form.Label>
-                    <input name="email" type="text" className="form-control" />
-                  </Form.Group>
-                  <Form.Group>
-                    <Form.Label>Password</Form.Label>
-                    <input name="password" type="password" className="form-control" />
-                  </Form.Group>
-                  <Button type="submit" className="mt-3">
-                    Signin
-                  </Button>
-                </Form>
-              </Card.Body>
-              <Card.Footer>
-                Don&apos;t have an account?
-                <a href="/auth/signup">Sign up</a>
-              </Card.Footer>
-            </Card>
+            <h1 className="text-center text-font">Welcome to Project Janus</h1>
+            <br />
+            <CardGroup>
+              <Card>
+                <Card.Body>
+                  <h3 className="text-center text-font">Sign In</h3>
+                  <Form method="post" onSubmit={handleSubmit}>
+                    <Form.Group controlId="formBasicEmail">
+                      <Form.Label className="text-font">Email</Form.Label>
+                      <input name="email" type="text" className="form-control" />
+                    </Form.Group>
+                    <Form.Group>
+                      <br />
+                      <Form.Label className="text-font">Password</Form.Label>
+                      <input name="password" type="password" className="form-control" />
+                    </Form.Group>
+                    <Button type="submit" className="mt-3 text-font" variant="success">
+                      Sign In
+                    </Button>
+                  </Form>
+                </Card.Body>
+              </Card>
+              <Card className="green-bg white-text">
+                <Card.Body>
+                  <br />
+                  <br />
+                  <h3 className="text-center text-font">Don&apos;t have an account?</h3>
+                  <Card.Text className="text-center text-font">
+                    <Button variant="light" href="/auth/signup">
+                      Sign Up
+                    </Button>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </CardGroup>
           </Col>
         </Row>
       </Container>
