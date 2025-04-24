c9b5bf383f669f1a290d5925dd61e79473b3331f
rycejrvy <goat.rj23@gmail.com>
2025-04-23 17:04:10 -1000

    Converted student profile mockup into React-Bootstrap

--- src/app/student-profile/page.tsx
+++ src/app/student-profile/page.tsx
@@ -2,12 +2,67 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/img-redundant-alt */
 /* eslint-disable max-len */
-/* import * as rbs from 'react-bootstrap'; */
+
+import { getServerSession } from 'next-auth';
+import { loggedInProtectedPage } from '@/lib/page-protection';
+import authOptions from '@/lib/authOptions';
+import * as rbs from 'react-bootstrap';
 
 /** Render a list of stuff for the logged in user. */
 const StudentProfile = async () => {
+  // Protect the page, only logged in users can access it.
+  const session = await getServerSession(authOptions);
+  loggedInProtectedPage(
+    session as {
+      user: { email: string; id: string; randomKey: string };
+    } | null,
+  );
+
+  return (
+    <main>
+      <rbs.Container>
+        <rbs.Row className="mb-5 mt-5">
+          <rbs.Col md={4} className="profile-sidebar">
+            <rbs.Card className="p-3 shadow-sm profile-sidebar-content">
+              <img src="https://static-00.iconduck.com/assets.00/profile-default-icon-2048x2045-u3j7s5nj.png" alt="Profile Picture" className="img-fluid rounded-circle mb-3" style={{ width: '150px', height: '150px' }} />
+              <p>JOHN DOE</p>
+              <p>XYZ MAJOR</p>
+              <p>PREFERRED GEOGRAPHIC LOCATION</p>
+              <a href="#" style={{ width: '5%' }}>Edit Profile</a>
+            </rbs.Card>
+          </rbs.Col>
+          <rbs.Col md={8} className="profile-contents">
+            <rbs.Card className="p-3 shadow-sm">
+              <h5>ABOUT ME</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </rbs.Card>
+
+            <rbs.Row className="mt-4">
+              <rbs.Col md={7}>
+                <rbs.Card className="p-3 shadow-sm">
+                  <h5>INTERESTS-SKILLS</h5>
+                  <p>Lorem ipsum</p>
+                </rbs.Card>
+              </rbs.Col>
+
+              <rbs.Col md={5}>
+                <rbs.Card className="p-3 shadow-sm">
+                  <h5>PROFESSIONAL PORTFOLIO</h5>
+                  <a href="#">Lorem ipsum</a>
+                </rbs.Card>
+              </rbs.Col>
+            </rbs.Row>
+
+          </rbs.Col>
+        </rbs.Row>
+      </rbs.Container>
+    </main>
+  );
+};
+  /**
   <main className="container my-5">
     <div className="row mb-5">
+      <h1> Test </h1>
       <div className="col-md-4 profile-sidebar">
         <div className="card p-3 shadow-sm profile-sidebar-content">
           <img src="https://static-00.iconduck.com/assets.00/profile-default-icon-2048x2045-u3j7s5nj.png" alt="Profile Picture" className="img-fluid rounded-circle mb-3" style={{ width: '150px', height: '150px' }} />
@@ -41,7 +96,7 @@ const StudentProfile = async () => {
 
       </div>
     </div>
-  </main>;
-};
+  </main>
+  */
 
 export default StudentProfile;
