bd1b329b766ecde1f4b98d0ee004c0f53cd9d62d
IsaiahDelaCruz <zaexdc1120@gmail.com>
2025-05-01 19:38:11 -1000

    Update page.tsx

--- src/app/browsing-page/page.tsx
+++ src/app/browsing-page/page.tsx
@@ -1,6 +1,6 @@
 // pages/company-directory.tsx
 // import React from 'react';
-import '../global.css';
+import '../globals.css';
 
 const CompanyDirectory = async () => (
   <div className="wrapper">
