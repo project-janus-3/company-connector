e20028f9afb483b53f7c2b5900c4195a6aeb2cfe
efigart <efigart@hawaii.edu>
2025-05-06 09:33:50 -1000

    fixed import statements
    added { } to authOptions import

--- src/app/editstudentprofile/[id]/page.tsx
+++ src/app/editstudentprofile/[id]/page.tsx
@@ -2,7 +2,7 @@
 import { getServerSession } from 'next-auth';
 import { notFound } from 'next/navigation';
 import { StudentProfile } from '@prisma/client';
-import authOptions from '@/lib/authOptions';
+import { authOptions } from '@/lib/authOptions';
 import { loggedInProtectedPage } from '@/lib/page-protection';
 import { prisma } from '@/lib/prisma';
 import EditStudentProfile from '@/components/EditStudentProfile';
