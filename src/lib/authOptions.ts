ca0a9d7866a8f10cc7bcc95d509fdbea52ca9b38
efigart <efigart@hawaii.edu>
2025-05-07 12:51:08 -1000

    update authOptions.ts
    commented out line 61 - error: '/auth/error',

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -58,7 +58,7 @@ export const authOptions: NextAuthOptions = {
   pages: {
     signIn: '/auth/signin',
     signOut: '/auth/signout',
-    error: '/auth/error',
+    // error: '/auth/error',
   },
   callbacks: {
     async jwt({ token, user }) {
