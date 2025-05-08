19aa7bccb1b7991831a3e6f3d987a86478e53b42
rycejrvy <goat.rj23@gmail.com>
2025-05-08 03:13:47 -1000

    Changed implementation of the browsing page
    - The usage of the interface to hold data instead of directly holding data may have impacted the functionality of the browsing page.

--- src/components/CompaniesBrowse.tsx
+++ src/components/CompaniesBrowse.tsx
@@ -2,42 +2,14 @@
 
 /* eslint-disable import/extensions */
 
-import Card from 'react-bootstrap/Card';
-import { Row, Col } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { CompanyProfile } from '@prisma/client';
+import CompanyBrowseCard from './CompanyBrowseCard';
 
-const CompaniesBrowse = ({ companies }: { companies: any[] }) => (
+const CompaniesBrowse = ({ companies }: { companies: CompanyProfile[] }) => (
   <Row>
     {companies.map((company) => (
-      <Col key={company.id} md={4} className="mb-4">
-        <Card>
-          <Card.Img
-            variant="top"
-            src={company.companyPic}
-            alt={`${company.name} logo`}
-            style={{ height: '150px', objectFit: 'cover' }}
-          />
-          <Card.Body>
-            <Card.Title>{company.name}</Card.Title>
-            <Card.Text>
-              <strong>Overview:</strong>
-              <br />
-              {company.overview}
-            </Card.Text>
-            <Row>
-              <Col>
-                <strong>Location:</strong>
-                <br />
-                {company.location}
-              </Col>
-              <Col>
-                <strong>Contact:</strong>
-                <br />
-                {company.contact}
-              </Col>
-            </Row>
-          </Card.Body>
-        </Card>
-      </Col>
+      <CompanyBrowseCard key={company.id} company={company} />
     ))}
   </Row>
 );
