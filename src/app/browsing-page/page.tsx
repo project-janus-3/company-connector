066d2563aebfadf862bb1a4afe71271cd29ec9e7
rycejrvy <goat.rj23@gmail.com>
2025-05-08 02:14:29 -1000

    Changed method of listing companies in browsing page

--- src/app/browsing-page/page.tsx
+++ src/app/browsing-page/page.tsx
@@ -1,6 +1,7 @@
 import { prisma } from '@/lib/prisma';
 import '../globals.css';
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+import CompaniesBrowse from '@/components/CompaniesBrowse';
 
 const CompanyDirectory = async () => {
   interface Company {
@@ -38,40 +39,7 @@ const CompanyDirectory = async () => {
             <p>Discover companies, jobs, and partnerships tailored to your skills and interests.</p>
           </Col>
         </Row>
-        <Row>
-          {companies.map((company) => (
-            <Col key={company.id} md={4} className="mb-4">
-              <Card>
-                <Card.Img
-                  variant="top"
-                  src={company.companyPic}
-                  alt={`${company.name} logo`}
-                  style={{ height: '150px', objectFit: 'cover' }}
-                />
-                <Card.Body>
-                  <Card.Title>{company.name}</Card.Title>
-                  <Card.Text>
-                    <strong>Overview:</strong>
-                    <br />
-                    {company.overview}
-                  </Card.Text>
-                  <Row>
-                    <Col>
-                      <strong>Location:</strong>
-                      <br />
-                      {company.location}
-                    </Col>
-                    <Col>
-                      <strong>Contact:</strong>
-                      <br />
-                      {company.contact}
-                    </Col>
-                  </Row>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
+        <CompaniesBrowse companies={companies} />
       </Container>
     </main>
   );
