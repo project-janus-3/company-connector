553ac3b58f99e47b16f9c84c3ca5a4da2dc87ac8
IsaiahDelaCruz <zaexdc1120@gmail.com>
2025-05-08 19:00:16 -1000

    fixed company card and added background img

--- src/app/browsing-page/page.tsx
+++ src/app/browsing-page/page.tsx
@@ -1,53 +1,29 @@
+/* eslint-disable react/self-closing-comp */
 import { prisma } from '@/lib/prisma';
 import '../globals.css';
-import { Col, Container, Row } from 'react-bootstrap';
 import CompaniesBrowse from '@/components/CompaniesBrowse';
 import { CompanyProfile } from '@prisma/client';
 
 const CompanyDirectory = async () => {
-  /*
-  interface Company {
-    id: number;
-    name: string;
-    overview: string;
-    location: string;
-    contact: string;
-    companyPic: string;
-  }
-
-  let companies: Company[] = [];
-
-  try {
-    companies = await prisma.companyProfile.findMany({
-      select: {
-        id: true,
-        name: true,
-        overview: true,
-        location: true,
-        contact: true,
-        companyPic: true,
-      },
-    });
-  } catch (error) {
-    console.error('Error fetching companies:', error);
-  }
-  */
-
-  // Get all companies from the database
-  const companies: CompanyProfile[] = await prisma.companyProfile.findMany({
-  });
+  const companies: CompanyProfile[] = await prisma.companyProfile.findMany({});
 
   return (
     <main>
-      <Container>
-        <Row className="text-center my-4">
-          <Col>
-            <h1>Connecting Professionals to Opportunities</h1>
-            <p>Discover companies, jobs, and partnerships tailored to your skills and interests.</p>
-          </Col>
-        </Row>
-        <CompaniesBrowse companies={companies} />
-      </Container>
+      <section className="hero-section">
+        <div className="hero-overlay">
+
+        </div>
+        <div className="hero-content">
+          <h1>Connecting Professionals to Opportunities</h1>
+          <p>Discover companies, jobs, and partnerships tailored to your skills and interests.</p>
+        </div>
+      </section>
+
+      <div className="companies-container">
+        <div className="companies-grid">
+          <CompaniesBrowse companies={companies} />
+        </div>
+      </div>
     </main>
   );
 };
