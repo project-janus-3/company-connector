fa5da53455679b47c9114063226eb8571cf89409
rycejrvy <goat.rj23@gmail.com>
2025-05-08 17:52:48 -1000

    Reverted accidental change on editjob implementation

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import { Card, ListGroup } from 'react-bootstrap';
 import { Job } from '@prisma/client';
 
@@ -10,12 +11,16 @@ const JobCard = ({ job }: { job: Job }) => (
         <Card.Title>{job.position}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">
           {job.type}
+          {' '}
           &middot;
-          $
+          {' $'}
           {job.salary}
+          {' '}
           &middot;
+          {' '}
+          {' '}
           {job.openings}
-          openings
+          {' openings ' }
         </Card.Subtitle>
       </Card.Header>
       <Card.Body>
@@ -25,6 +30,11 @@ const JobCard = ({ job }: { job: Job }) => (
         </Card.Text>
         <Card.Text>{job.description}</Card.Text>
       </Card.Body>
+      <Card.Footer>
+        <Link href={`editjob/${job.id}`} className="btn btn-primary">
+          Edit Details
+        </Link>
+      </Card.Footer>
     </Card>
   </ListGroup.Item>
 );
